0f59569a7ea66c3810088097107b302bd9081786
Rebecca Rauschmayer <rar4@linux-ssh-05.ews.illinois.edu>
2021-12-12 13:03:02 -0600

    added clarifying text

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main() {
     bool TEST_STATE = true;
     
     if (DEMO_PAGERANK_STATE_) {
-        Graph g = Graph("large_twitch_features.csv", "large_twitch_edged.csv");
+        Graph g = Graph("large_twitch_features.csv", "large_twitch_edges.csv");
         Pagerank p = Pagerank();
         p.powerPageRank(g, 10);
     }
@@ -39,7 +39,7 @@ int main() {
         // FDG args: graph g, spring constant, spring rest length, coulomb constant, time step (delta T), max # of iterations, width and height of output
  	std::cout << "Setting up force-directed graph..." << std::endl;       
         f.setup(g, 5, 100., 100., 0.001, 1000, w, h);  // FDG parameters are fairly sensitive, equilibrium can usually be achieved with small spring const (~10^-2) and large coulomb (~10^4)
-
+	std::cout<<"Find PNG in FDG_out.png"<<std::endl;
     }
    
     
